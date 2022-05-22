f2bcd8190d0720b16619b201780c9e2fb8dd6911
mmartin3 <mmartin1121@gmail.com>
2022-05-22 01:21:12 -0400

    Update Drug.kt

--- java/com/mattmakesapps/mostrx/Drug.kt
+++ java/com/mattmakesapps/mostrx/Drug.kt
@@ -8,17 +8,17 @@ data class Drug(val brandName: String,
                 val selectedForm: DrugResponse.Form,
                 val selectedStrength: DrugResponse.Strength,
                 val selectedQuantity: DrugResponse.Quantity): Serializable {
-    val brandSlug: String = brandName.nameToSlug()
-    val genericSlug: String = genericName.nameToSlug()
-    val branding: Branding = if (selectedBrand.isGeneric) Branding.GENERIC else Branding.BRAND
+    val brandSlug = brandName.nameToSlug()
+    val genericSlug = genericName.nameToSlug()
+    val branding = if (selectedBrand.isGeneric) Branding.GENERIC else Branding.BRAND
     val slug = if (branding == Branding.BRAND) brandSlug else genericSlug
-    val form: String = selectedForm.name
-    val encodedForm: String = form.encodeSlashes()
-    val dosage: String = selectedStrength.strength
-    val encodedDosage: String = dosage.encodeSlashes()
-    val quantity: String = selectedQuantity.amt.toString()
+    val form = selectedForm.toString()
+    val encodedForm = form.encodeSlashes()
+    val dosage = selectedStrength.toString()
+    val encodedDosage = dosage.encodeSlashes()
+    val quantity = selectedQuantity.amt.toString()
     val displayQuantity = if (form == "Bottle") "1 Bottle" else "$quantity ${encodedForm}s"
-    val ndc: String = selectedQuantity.ndc
+    val ndc = selectedQuantity.ndc.toString()
 
     fun getDisplayName(parenthetical: Boolean = false): String {
         return when {
@@ -35,7 +35,7 @@ data class Drug(val brandName: String,
     }
 
     private fun String.encodeSlashes(): String {
-        return replace("/", "%2F");
+        return replace("/", "%2F")
     }
 
     private fun String.nameToSlug(): String {
